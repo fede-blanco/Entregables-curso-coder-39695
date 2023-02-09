ab294655b31b309412875bd9398982c0db80ca32
fede-blanco <blancofedem@gmail.com>
2023-02-09 09:50:54 -0300

    Saco algunos console.log()

--- entregable_clase_6/src/app.js
+++ entregable_clase_6/src/app.js
@@ -11,7 +11,6 @@ app.use(express.urlencoded({ extended: true })); //Permite enviar info desde la 
 
 //Controla la acción de la url "/"
 async function homeController(req, res) {
-  console.log("hola desde GET HOME");
   res.send("Hola desde GET HOME");
 }
 
@@ -19,13 +18,10 @@ async function homeController(req, res) {
 async function productsController(req, res) {
   //En caso de que haya algo en la propiedad query.limit recorta el array de acuerdo con ese limite
   if (req.query.limit) {
-    console.log("Hola desde GET PRODUCTS ? LIMIT");
-    console.log(req.query.limit);
     const productsAdquired = await productManager.getProducts();
     const productsWhitLimit = productsAdquired.slice(0, req.query.limit);
     res.json(productsWhitLimit);
   } else {
-    console.log("Hola desde GET PRODUCTS");
     const productsAdquired = await productManager.getProducts();
     res.json(productsAdquired);
   }
@@ -33,7 +29,6 @@ async function productsController(req, res) {
 
 //Controla la acción de la url "/products" con param :id
 async function productByIdController(req, res) {
-  console.log(`Hola desde ProductById -- id: ${req.params.id}`);
   const productAdquired = await productManager.getProductById(
     //Fue imprescindible parsear a número el id recibido por params para poder utilizarlo en el método getProductById, sino undefined
     parseInt(req.params.id)
